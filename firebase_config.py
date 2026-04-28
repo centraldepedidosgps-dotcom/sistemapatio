2b0e848384441ccb83b522124b01c933e8dcd57d
centraldepedidosgps-dotcom <centraldepedidosgps@gmail.com>
2026-04-28 14:08:22 -0300

    firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,12 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
 
-cred = credentials.Certificate("firebase_key.json")
+firebase_json = os.environ.get("FIREBASE_KEY")
+
+cred = credentials.Certificate(json.loads(firebase_json))
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
