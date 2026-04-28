302ed92ec6df8c8237dd8ed99bc0d94e58f0139c
centraldepedidosgps-dotcom <centraldepedidosgps@gmail.com>
2026-04-28 13:52:39 -0300

    firebase_auth.py

--- firebase_auth.py
+++ firebase_auth.py
@@ -7,9 +7,7 @@ config = {
     "storageBucket": "sistemapatio-5fbe5.appspot.com",
     "messagingSenderId": "953703344743",
     "appId": "1:953703344743:web:0b2ad418f9004a325702c2",
-
-    # 🔥 ISSO AQUI É O QUE FALTA
-    "databaseURL": ""
+    "databaseURL": "https://sistemapatio-5fbe5.firebaseio.com"
 }
 
 firebase = pyrebase.initialize_app(config)
