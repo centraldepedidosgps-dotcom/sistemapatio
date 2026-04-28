a94df826a90710afc24d2e5fe905fe26a773bc5a
centraldepedidosgps-dotcom <centraldepedidosgps@gmail.com>
2026-04-28 14:31:35 -0300

    firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,17 +1,12 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
-import json
 
+# Pega a chave do Firebase do ambiente
 firebase_json = os.environ.get("FIREBASE_KEY")
-
-if not firebase_json:
-    raise Exception("FIREBASE_KEY não encontrada nas variáveis de ambiente do Render")
-
-cred = credentials.Certificate(json.loads(firebase_json))
-
-# evita erro se já inicializou (Render reloads podem duplicar init)
 if not firebase_admin._apps:
+    cred = credentials.Certificate(json.loads(firebase_json))
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
